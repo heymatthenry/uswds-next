4ae97696bfac14beb4764e24f62e9968b36bfa5f
Matt Henry <matt.henry@gsa.gov>
2022-05-04 13:57:39 -0400

    wip basic uswds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [
+      path.join(__dirname, 'styles'),
+      "./node_modules/@uswds",
+      "./node_modules/@uswds/uswds/packages"
+    ],
+  },
 }
 
 module.exports = nextConfig
